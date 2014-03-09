4fab58451bb44b57c0cfd883ec1c16a46b55a97d
Alejandro Garcia Andrade <garciaae@MacBook-3.local>
2014-03-09 20:01:14 +0100

    Initial Commit

---
+++ PathAct/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PathAct
+//
+//  Created by Alejandro Garcia Andrade on 09/03/14.
+//  Copyright (c) 2014 Alejandro Garcia Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "pathActAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([pathActAppDelegate class]));
+    }
+}
---
+++ PathAct/pathActAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  pathActAppDelegate.h
+//  PathAct
+//
+//  Created by Alejandro Garcia Andrade on 09/03/14.
+//  Copyright (c) 2014 Alejandro Garcia Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pathActAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
